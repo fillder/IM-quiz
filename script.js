3b0ad5805feb4b4d140f770623b66b66daaab80f
Marius S. Riseth <marius.s.riseth@pm.me>
2023-02-06 21:42:49 +0100

    Funket dårlig

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let score = 0;
-const totalQuestions = 6; //endre dette antallet dersom dere ønsker flere spørsmål (inkluder "start quiz" som om det er et spørmål)
-const questions = [
+let questionNumber = 0;
+const quizQuestions = [
   { //Dette er kodet som et spørsmål, men er egntlig kun satt opp for at dere skal slippe å lage en unik start
     options: [
       { text: "Start Quizen!", correct: false}, 
@@ -61,43 +60,145 @@ const questions = [
 
 
 //___________________________Dere trenger ikke redigere noe under denne linjen_______________________________//
-let currentQuestion = 0;
 
-const handleOptionClick = (isCorrect) => {
-  if (isCorrect) {
+function handleOptionClick(event) {
+  var selectedOption = event.target;
+  var answer = selectedOption.textContent;
+  var question = quizQuestions[currentQuestionIndex];
+  if (answer === question.correctAnswer) {
     score++;
   }
 
-  if (currentQuestion === totalQuestions - 1) {
-    // Hvis sluttresultat
-    const resultContainer = document.querySelector("#resultContainer");
-    resultContainer.innerHTML = `Your final score is: ${score}/${questions.length-1}`;
-    resultContainer.style.display = "block"; //Viser resultatteksten etter at siste spørsmål er besvart
-    questionContainer.style.display = "none"; //skjuler spørsmålsboksen etter at siste spørsmål er besvart
-  } else {
-    currentQuestion++;
-    renderQuestion();
+  if (currentQuestionIndex === questions.length - 1) {
+    displayResults();
+    return;
   }
-};
 
-const renderQuestion = () => {
-  const questionContainer = document.querySelector("#questionContainer");
-  const currentQ = questions[currentQuestion];
+  currentQuestionIndex++;
+  displayQuestion();
+}
+
+const showQuestion = () => {
+  if (questionNumber >= quizQuestions.length) {
+    showResults();
+    return;
+  }
+  const question = quizQuestions[questionNumber];
+  const questionContainer = document.getElementById('questionContainer');
   questionContainer.innerHTML = `
-    <h3>${currentQ.question}</h3>
-    <img src="${questions[currentQuestion].imageUrl}" alt="Question Image">
-    <div>
-      ${currentQ.options
-        .map(
-          (option, index) => `
-        <button onclick="handleOptionClick(${option.correct})">
-          ${option.text}
-        </button>
-      `
-        )
-        .join("")}
-    </div>
+    <p>${question.question}</p>
+    ${question.options.map((option, index) => `
+      <div>
+        <input type="radio" name="answer" id="answer${index}" value="${index}">
+        <label for="answer${index}">${option}</label>
+      </div>
+    `).join('')}
+    <button type="button">Neste</button>
   `;
 };
 
-renderQuestion();
+const showResults = () => {
+  const resultContainer = document.getElementById('resultContainer');
+  let result = 0;
+  const answers = Array.from(document.querySelectorAll('input[name="answer"]:checked')).map(input => input.value);
+  quizQuestions.forEach((question, index) => {
+    if (answers[index] == question.correctOption) {
+      result++;
+    }
+  });
+  resultContainer.innerHTML = `
+    <p>Du fikk ${result} av ${quizQuestions.length} riktige.</p>
+    <button type="button">Prøv igjen</button>
+  `;
+};
+
+const handleNextButtonClick = (event) => {
+  const target = event.target;
+  if (target.tagName === 'BUTTON') {
+    questionNumber++;
+    showQuestion();
+  }
+};
+
+document.addEventListener('click', handleNextButtonClick);
+
+const handleClick = () => {
+  setLoading(true);
+  fetchData(inputValue)
+    .then((data) => {
+      setData(data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setLoading(false);
+      console.error(error);
+    });
+};
+
+return (
+  <div>
+    <input
+      type="text"
+      value={inputValue}
+      onChange={(event) => setInputValue(event.target.value)}
+    />
+    <button disabled={loading} onClick={handleClick}>
+      Fetch Data
+    </button>
+    {loading && <div>Loading...</div>}
+    {data && (
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
+    )}
+  </div>
+);
+
+
+// function showResult() {
+//   const resultContainer = document.getElementById("resultContainer");
+//   resultContainer.innerHTML = "";
+
+//   const resultElement = document.createElement("p");
+//   resultElement.innerText = `Du fikk ${score} av ${questions.length} riktige svar!`;
+//   resultContainer.appendChild(resultElement);
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   showQuestion();
+// });
+
+
+//   if (currentQuestion === totalQuestions - 1) {
+//     // Hvis sluttresultat
+//     const resultContainer = document.querySelector("#resultContainer");
+//     resultContainer.innerHTML = `Your final score is: ${score}/${questions.length-1}`;
+//     resultContainer.style.display = "block"; //Viser resultatteksten etter at siste spørsmål er besvart
+//     questionContainer.style.display = "none"; //skjuler spørsmålsboksen etter at siste spørsmål er besvart
+//   } else {
+//     currentQuestion++;
+//     renderQuestion();
+//   }
+// };
+
+// const renderQuestion = () => {
+//   let questionContainer = document.querySelector("#questionContainer");
+//   const currentQ = questions[currentQuestion];
+//   questionContainer.innerHTML = `
+//     <h3>${currentQ.question}</h3>
+//     <img src="${questions[currentQuestion].imageUrl}" alt="Question Image">
+//     <div>
+//       ${currentQ.options
+//         .map(
+//           (option, index) => `
+//         <button onclick="handleOptionClick(${option.correct})">
+//           ${option.text}
+//         </button>
+//       `
+//         )
+//         .join("")}
+//     </div>
+//   `;
+// };
+
+// renderQuestion();
