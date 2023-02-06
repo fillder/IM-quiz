56a8c7c83a57be3468bfdf41c0171efc2d8d37e5
Marius S. Riseth <marius.s.riseth@pm.me>
2023-02-06 15:33:40 +0100

    Klar til bruk

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let score = 0;
-const totalQuestions = 5; //endre til et større antall om dere ønsker flere enn 5
+const totalQuestions = 6; //endre dette antallet dersom dere ønsker flere spørsmål (inkluder "start quiz" som om det er et spørmål)
 const questions = [
+  { //Dette er kodet som et spørsmål, men er egntlig kun satt opp for at dere skal slippe å lage en unik start
+    options: [
+      { text: "Start Quizen!", correct: false}, 
+    ]
+  },
   {
     question: "[Skriv spørsmål 1 her]?",
     options: [
-      { text: "Alternativ 1", correct: false },
-      { text: "Alternativ 2", correct: true }, //Det eller de svarene som får "true" bak seg, gir poeng til brukerne
-      { text: "Alternativ 3", correct: false }
+      { text: "Alternativ 1", correct: true }, //"true" = riktig svar. Skriv det inn i ulike linjer for å endre hvilken knapp som er riktig
+      { text: "Alternativ 2", correct: false }, //Legg til eller fjern så mange svaralternativ som dere ønsker.
+      { text: "Alternativ 3", correct: false } //Siste spørsmålslinje skal ikke ha komma.
     ],
-    imageUrl: 'https://unsplash.it/400/200', //bytt ut med en ny bildeaddresse for hvert spørsmål
+    imageUrl: 'https://unsplash.it/400/200', //bytt ut med en ny bildeaddresse for hvert spørsmål. 400 og 200 er størrelsen i pixler.
     altText: 'Beskrivelse av bilde'
   },
   {
     question: "[Skriv spørsmål 2 her]?",
     options: [
-      { text: "Alternativ I", correct: false },
-      { text: "Alternativ II", correct: true },
+      { text: "Alternativ I", correct: true },
+      { text: "Alternativ II", correct: false },
       { text: "Alternativ III", correct: false }
     ],
     imageUrl: 'https://unsplash.it/400/200',
@@ -24,8 +29,8 @@ const questions = [
   {
     question: "[Skriv spørsmål 3 her]?",
     options: [
-      { text: "Alternativ A", correct: false },
-      { text: "Alternativ B", correct: true },
+      { text: "Alternativ A", correct: true },
+      { text: "Alternativ B", correct: false },
       { text: "Alternativ C", correct: false }
     ],
     imageUrl: 'https://unsplash.it/400/200',
@@ -34,8 +39,8 @@ const questions = [
   {
     question: "[Skriv spørsmål 4 her]?",
     options: [
-      { text: "Alternativ .", correct: false },
-      { text: "Alternativ ..", correct: true },
+      { text: "Alternativ .", correct: true },
+      { text: "Alternativ ..", correct: false },
       { text: "Alternativ ...", correct: false }
     ],
     imageUrl: 'https://unsplash.it/400/200',
@@ -44,16 +49,18 @@ const questions = [
   {
     question: "[Skriv spørsmål 5 her]?",
     options: [
-      { text: "Alternativ -", correct: false },
-      { text: "Alternativ +", correct: true },
+      { text: "Alternativ -", correct: true },
+      { text: "Alternativ +", correct: false },
       { text: "Alternativ x", correct: false }
     ],
     imageUrl: 'https://unsplash.it/400/200',
     altText: 'Beskrivelse av bilde'
   },
-  // Legg til flere spørsmål her
+  // Kopier malen over for å legge til flere spørsmål. Husk å endre antall spørsmål øverst.
 ];
 
+
+//___________________________Dere trenger ikke redigere noe under denne linjen_______________________________//
 let currentQuestion = 0;
 
 const handleOptionClick = (isCorrect) => {
@@ -64,8 +71,9 @@ const handleOptionClick = (isCorrect) => {
   if (currentQuestion === totalQuestions - 1) {
     // Hvis sluttresultat
     const resultContainer = document.querySelector("#resultContainer");
-    resultContainer.innerHTML = `Your final score is: ${score}/${totalQuestions}`;
-    resultContainer.style.display = "block";
+    resultContainer.innerHTML = `Your final score is: ${score}/${questions.length-1}`;
+    resultContainer.style.display = "block"; //Viser resultatteksten etter at siste spørsmål er besvart
+    questionContainer.style.display = "none"; //skjuler spørsmålsboksen etter at siste spørsmål er besvart
   } else {
     currentQuestion++;
     renderQuestion();
@@ -76,7 +84,7 @@ const renderQuestion = () => {
   const questionContainer = document.querySelector("#questionContainer");
   const currentQ = questions[currentQuestion];
   questionContainer.innerHTML = `
-    <h2>${currentQ.question}</h2>
+    <h3>${currentQ.question}</h3>
     <img src="${questions[currentQuestion].imageUrl}" alt="Question Image">
     <div>
       ${currentQ.options
